10fe8620dcd73355eb39f8960d4a046b7501b75a
Jonathan Mak <j.m4k119@gmail.com>
2023-06-06 15:55:50 -0400

    server commit

--- server.js
+++ server.js
@@ -7,13 +7,26 @@ app.use(cors({ origin: process.env.CORS_ORIGIN }));
 
 app.use(express.json());
 
-const inventoryRoutes = require("./routes/inventory");
-app.use("/inventory", inventoryRoutes);
+// const inventoryRoutes = require("./routes/inventory");
+// app.use("/inventory", inventoryRoutes);
 
-const warehouseRoutes = require("./routes/warehouse");
-app.use("/warehouse", warehouseRoutes);
-
+// const warehouseRoutes = require("./routes/warehouse");
+// app.use("/warehouse", warehouseRoutes);
+const knex = require("knex")(require("./knexfile"));
 // LISTENER
 app.listen(8080, () => {
   console.log("Listening on 8080");
+});
+
+// GET WAREHOUSE DATA
+app.get("/warehouses", (req, res) => {
+  knex
+    .select("*")
+    .from("warehouses")
+    .then((data) => {
+      res.json(data);
+    })
+    .catch((err) => {
+      res.status(500).send("Error getting warehouses");
+    });
 });
