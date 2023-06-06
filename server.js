939e945df5f7b2ef13b1b57ed45be429748089f4
Jesse Uszkay <jesseuszkay@Jims-MBP.localdomain>
2023-06-06 11:05:14 -0400

    Create database, migrate and seed tables, check get request

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express();
 const cors = require("cors");
 require("dotenv").config();
 const port = process.env.PORT || 8080;
+
 app.use(cors({ origin: process.env.CORS_ORIGIN }));
 
 app.use(express.json());
@@ -13,7 +14,22 @@ app.use("/inventory", inventoryRoutes);
 const warehouseRoutes = require("./routes/warehouse");
 app.use("/warehouse", warehouseRoutes);
 
+const knex = require("knex")(require("./knexfile"));
+
 // LISTENER
-app.listen(8080, () => {
+app.listen(PORT, () => {
   console.log("Listening on 8080");
+});
+
+// GET WAREHOUSE DATA
+app.get("/warehouses", (req, res) => {
+  knex
+    .select("*")
+    .from("warehouses")
+    .then((data) => {
+      res.json(data);
+    })
+    .catch((err) => {
+      res.status(500).send("Error getting warehouses");
+    });
 });
