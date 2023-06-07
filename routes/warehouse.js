eed9a0523baa7b71004577f8ea72c9f26fcc8997
Jonathan Mak <j.m4k119@gmail.com>
2023-06-07 15:19:13 -0400

    add staus 200 to send a response with 200 status code

--- routes/warehouse.js
+++ routes/warehouse.js
@@ -3,11 +3,11 @@ const knex = require("knex")(require("../knexfile"));
 
 // GET WAREHOUSE DATA
 router.get("/", (req, res) => {
-  knex
+  knex("warehouses")
     .select("*")
     .from("warehouses")
     .then((data) => {
-      res.json(data);
+      res.status(200).json(data);
     })
     .catch((err) => {
       res.status(500).send("Error getting warehouses");
