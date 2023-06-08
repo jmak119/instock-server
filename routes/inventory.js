130dc7a3025169f939c3701f2aae752cc93750ce
Ajay Singh <contact@ajaysingh.ca>
2023-06-08 14:12:42 -0400

    add /warehouse/:id for inventory

--- routes/inventory.js
+++ routes/inventory.js
@@ -34,6 +34,26 @@ router.get("/:id", (req, res) => {
     });
 });
 
+// Get Inventory Items by Warehouse ID
+router.get("/warehouse/:id", (req, res) => {
+  knex("inventories")
+    .where({ warehouse_id: req.params.id })
+    .then((itemsFound) => {
+      if (itemsFound.length === 0) {
+        return res.status(404).json({
+          message: `Inventory Item with Warehouse ID: ${req.params.id} not found`,
+        });
+      }
+      res.status(200).json(itemsFound);
+    })
+    .catch(() => {
+      res.status(500).json({
+        message: `Unable to retrieve inventory item data for item with Warehouse ID: ${req.params.id}`,
+      });
+    });
+});
+
+
 // PUT/EDIT INVENTORY ITEM DATA
 router.put("/:id", (req, res) => {
   if (!req.body.item_name || !req.body.description) {
